ae7e5b619b9cf9bef3284c681cc19cb1cc4bfde5
PedroHercules0810 <pedrosouzagrangeiro@gmail.com>
2023-04-27 15:22:15 -0300

    criando o pharm_insere

--- pharmacy.c
+++ pharmacy.c
@@ -5,19 +5,18 @@
 #include "med.c"
 #include <ctype.h>
 
-struct pharmacy {
-char nome[50];
-int codigo;
-char localizacao[50];
-int num_med;
-char horario[50];
-Med * med;
-Pharm * ant;
-Pharm * prox;
+struct pharmacy
+{
+    char nome[50];
+    int codigo;
+    char localizacao[50];
+    int num_med;
+    char horario[50];
+    Med *med;
+    Pharm *ant;
+    Pharm *prox;
 };
 typedef struct pharmacy Pharm;
-
-
 
 void combSort(char lista[][50], int n)
 {
@@ -49,7 +48,7 @@ void combSort(char lista[][50], int n)
         }
     }
 
-abrir = fopen("pharmacy.txt", "wt");
+    abrir = fopen("pharmacy.txt", "wt");
 
     while (controle < n)
     {
@@ -84,9 +83,10 @@ int contador()
     return (numLinhas);
 }
 
-Pharm* pharm_insere(Pharm *p, char name[50], int cod, char loc[50], char horario[50]){
+Pharm *pharm_insere(Pharm *p, char name[50], int cod, char loc[50], char horario[50])
+{
 
-    Pharm* novo = (Pharm*) malloc(sizeof(Pharm));
+    Pharm *novo = (Pharm *)malloc(sizeof(Pharm));
     *novo->nome = name;
     novo->codigo = cod;
     *novo->localizacao = loc;
@@ -94,42 +94,47 @@ Pharm* pharm_insere(Pharm *p, char name[50], int cod, char loc[50], char horario
     *novo->horario = horario;
     novo->prox = p;
     novo->ant = NULL;
-    if(p!=NULL)
+    if (p != NULL)
         p->ant = novo;
     return novo;
 }
 
-
-void lst_imprime(Lista2*l){
-	Lista2*p;
-	for(p=l; p!=NULL; p=p->prox){
-		printf(" Info = %d \n", p->info);
-	}
-
+void pharm_imprime(Pharm *l)
+{
+    Pharm *p;
+    for (p = l; p != NULL; p = p->prox)
+    {
+        printf(" Código da farmacia = %d \n", p->codigo);
+        printf(" Nome da farmacia = %d \n", p->nome);
+        printf(" Localização da farmacia = %d \n", p->localizacao);
+        printf(" Estoque da farmacia = %d \n", p->num_med);
+        printf(" Horario de funcionamento = %d \n", p->horario);
+    }
 }
 
-Lista2* lst_busca(Lista2 *l, int v){
+Lista2 *lst_busca(Lista2 *l, int v)
+{
 
     Lista2 *p;
-    for(p=l; p!=NULL; p=p->prox){
-        if(p->info ==v)
-			return p;
+    for (p = l; p != NULL; p = p->prox)
+    {
+        if (p->info == v)
+            return p;
     }
     return NULL;
-
 }
 
-Lista2* lst_retira(Lista2 *l, int v){
-    Lista2* p = lst_busca(l,v);
+Lista2 *lst_retira(Lista2 *l, int v)
+{
+    Lista2 *p = lst_busca(l, v);
 
-    if(p->prox!=NULL)
-        p->prox->ant=p->ant;
-    if(l==NULL)
+    if (p->prox != NULL)
+        p->prox->ant = p->ant;
+    if (l == NULL)
         return l;
-    if(p==l)
-        l=p->prox;
+    if (p == l)
+        l = p->prox;
     else
         p->ant->prox = p->prox;
     return l;
-
 }
